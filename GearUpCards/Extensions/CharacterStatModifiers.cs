e9fafeb7a24f6956950f8c72c8f93e642d8c5e20
Wachirawit Soontornworachan <pud_wachirawit@hotmail.com>
2022-02-26 06:35:00 +0700

    Implemented ...GearData field into CharacterStatModifiers

---
+++ GearUpCards/Extensions/CharacterStatModifiers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+
+
+namespace GearUpCards.Extensions
+{
+    // Add fields to CharacterStatModifiers
+    // Using PCE's extension as coding reference
+
+    public class CharacterStatModifiersGearData
+    {
+        public int hollowLifeStack;
+
+        public CharacterStatModifiersGearData()
+        {
+            hollowLifeStack = 0;
+        }
+    }
+
+    public static class CharacterStatModifiersGearDataExtensions
+    {
+        public static readonly ConditionalWeakTable<CharacterStatModifiers, CharacterStatModifiersGearData> data =
+            new ConditionalWeakTable<CharacterStatModifiers, CharacterStatModifiersGearData>();
+
+        public static CharacterStatModifiersGearData GetGearData(this CharacterStatModifiers characterStat)
+        {
+            return data.GetOrCreateValue(characterStat);
+        }
+
+        public static void AddData(this CharacterStatModifiers characterStat, CharacterStatModifiersGearData value)
+        {
+            try
+            {
+                data.Add(characterStat, value);
+            }
+            catch (Exception) { }
+        }
+    }
+
+    [HarmonyPatch(typeof(CharacterStatModifiers), "ResetStats")]
+    class CharacterStatModifiersPatchResetStats
+    {
+        private static void Prefix(CharacterStatModifiers __instance)
+        {
+            __instance.GetGearData().hollowLifeStack = 0;
+        }
+    }
+}
