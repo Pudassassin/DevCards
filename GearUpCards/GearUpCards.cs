4e44b9bf309c71c30e245eeb6aa6d7e241744a17
Wachirawit Soontornworachan <pud_wachirawit@hotmail.com>
2022-02-26 16:13:18 +0700

    Functional [Hollow Life] card

--- GearUpCards/GearUpCards.cs
+++ GearUpCards/GearUpCards.cs
@@ -22,7 +22,7 @@ namespace GearUpCards
     {
         private const string ModId = "com.pudassassin.rounds.GearUpCards";
         private const string ModName = "GearUpCards";
-        public const string Version = "0.0.14";
+        public const string Version = "0.0.19";
 
         public const string ModInitials = "Gear Up";
 
--- GearUpCards/MonoBehaviours/HollowLifeEffect.cs
+++ GearUpCards/MonoBehaviours/HollowLifeEffect.cs
@@ -2,36 +2,61 @@
 using UnboundLib;
 using UnityEngine;
 using System.Linq;
-using UnboundLib.Networking;
+using UnboundLib.GameModes;
 using Photon.Pun;
 using System.Reflection;
 using ModdingUtils.MonoBehaviours;
 
 using GearUpCards.Extensions;
+using System.Collections;
 
 namespace GearUpCards.MonoBehaviours
 {
-    internal class HollowLifeEffect : ReversibleEffect
+    internal class HollowLifeEffect : MonoBehaviour
     {
         private const float healthCapFactor = .70f;
 
         private const float healthCullRate = .05f;
         private const float procTime = .10f;
 
-        private int stackCount = 0;
-        float healthCapPercentage = 1.0f;
+        internal int stackCount = 0;
+        internal float healthCapPercentage = 1.0f;
+        internal float timer = 0.0f;
+        internal bool effectWarmup = true;
+        internal bool effectEnabled = false;
 
-        float timer = 0.0f;
+        internal Player player;
+        internal CharacterStatModifiers stats;
 
-        private Player player;
-        private CharacterStatModifiers stats;
+        /* DEBUG */
+        // internal int proc_count = 0;
 
-        override public void OnAwake()
+
+        public void Awake()
         {
             this.player = this.gameObject.GetComponent<Player>();
+            this.stats = this.gameObject.GetComponent<CharacterStatModifiers>();
+
+            GameModeManager.AddHook(GameModeHooks.HookPointStart, OnPointStart);
+            GameModeManager.AddHook(GameModeHooks.HookBattleStart, OnBattleStart);
+            GameModeManager.AddHook(GameModeHooks.HookPointEnd, OnPointEnd);
         }
 
-        override public void OnUpdate()
+        public void Start()
+        {
+        }
+
+        public void FixedUpdate()
+        {
+            // Attempt to make hard HP Capped reliable and at foremost of the updates
+            if (effectEnabled && player.data.HealthPercentage > .80f && stackCount > 0)
+            {
+                // hard cap to prevent [Pristine Perserverence] to become active and mess things up
+                player.data.health = player.data.maxHealth * .80f;
+            }
+        }
+
+        public void Update()
         {
             timer += Time.deltaTime;
 
@@ -41,36 +66,97 @@ namespace GearUpCards.MonoBehaviours
                 // [?] EXC's [Second Wind] implement it differently and seem to either look for health removal or it tries its best to heal up to said health point
                 // [!!] HDC's [Holy Light] directly compare current health with previous health each Update() calls and 'charge' on positive gains
                 //      leading to it having massive charge at round starts, and will accumulate on each of ebb and flow of health now incurred by [Hollow Life]
-                stackCount = base.characterStatModifiers.GetGearData().hollowLifeStack;
+                // [!!] This card is dis-synergistic with [Pristine Perserverence] and any issue with it is considered as edge cases
+                stackCount = stats.GetGearData().hollowLifeStack;
                 RecalculateHealthCap();
 
-                if (!player.data.dead && !player.data.healthHandler.isRespawning && stackCount > 0)
-                {
-                    if (player.data.HealthPercentage > .85f)
-                    {
-                        player.data.health = player.data.maxHealth * .85f;
-                    }
-                    else if (player.data.HealthPercentage > healthCapPercentage)
-                    {
-                        float healthCullPercentage = Mathf.Clamp(player.data.HealthPercentage - healthCapPercentage, 0.0f, healthCullRate);
-                        player.data.health -= player.data.maxHealth * healthCullPercentage;
+                // if (proc_count >= 10)
+                // {
+                //     UnityEngine.Debug.Log($"[HOLLOW] running on player [{player.playerID}] with [{stackCount}] stacks");
+                //     proc_count = 0;
+                // }
 
+                if (effectEnabled && stackCount > 0)
+                {
+                    if (player.data.HealthPercentage > healthCapPercentage)
+                    {
+                        float healthCullPercentage = Mathf.Clamp(player.data.HealthPercentage - healthCapPercentage, 0.0f, healthCullRate * stackCount);
+                        player.data.health -= player.data.maxHealth * healthCullPercentage;
                     }
+
+                    // UnityEngine.Debug.Log($"[HOLLOW] culled player [{player.playerID}] HP");
                 }
 
                 timer -= procTime;
+                // proc_count++;
+            }
+
+            if (effectWarmup && stackCount > 0)
+            {
+                // resolving with [Pristine Perserverence] at point start
+                player.data.health = player.data.maxHealth * .80f;
             }
         }
 
-        override public void OnOnDestroy()
+        private IEnumerator OnPointStart(IGameModeHandler gm)
         {
+            effectWarmup = true;
+            effectEnabled = false;
 
+            yield break;
+        }
+
+        private IEnumerator OnBattleStart(IGameModeHandler gm)
+        {
+            effectWarmup = false;
+            effectEnabled = true;
+
+            stackCount = stats.GetGearData().hollowLifeStack;
+            RecalculateHealthCap();
+
+            // UnityEngine.Debug.Log($"[HOLLOW] from player [{player.playerID}] - Battle Start");
+
+            yield break;
+        }
+
+        private IEnumerator OnPointEnd(IGameModeHandler gm)
+        {
+            effectEnabled = false;
+        
+            // UnityEngine.Debug.Log($"[HOLLOW] from player [{player.playerID}] - Point End");
+        
+            yield break;
+        }
+
+        public void OnDisable()
+        {
+            bool isRespawning = player.data.healthHandler.isRespawning;
+            // UnityEngine.Debug.Log($"[HOLLOW] from player [{player.playerID}] - is resurresting [{isRespawning}]");
+
+            if (isRespawning)
+            {
+                // does nothing
+                // UnityEngine.Debug.Log($"[HOLLOW] from player [{player.playerID}] - is resurresting!?");
+            }
+            else
+            {
+                effectEnabled = false;
+                // UnityEngine.Debug.Log($"[HOLLOW] from player [{player.playerID}] - dead ded!?");
+            }
+        }
+
+        public void OnDestroy()
+        {
+            // This effect should persist between rounds, and at 0 stack it should do nothing mechanically
+
+            GameModeManager.RemoveHook(GameModeHooks.HookPointStart, OnPointStart);
+            GameModeManager.RemoveHook(GameModeHooks.HookBattleStart, OnBattleStart);
+            GameModeManager.RemoveHook(GameModeHooks.HookPointEnd, OnPointEnd);
         }
 
         public void RecalculateHealthCap()
         {
             this.healthCapPercentage = Mathf.Pow(healthCapFactor, stackCount);
         }
-
     }
 }
